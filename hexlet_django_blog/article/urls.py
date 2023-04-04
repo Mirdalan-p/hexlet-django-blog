badf8e736111957d7eda76ff9c22aef6add54de0
Mirdalan <mirdalan.p@gmail.com>
2023-04-04 21:07:22 +0300

    added 'edit' functional

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView
-from hexlet_django_blog.article.views import ArticleCommentsView
+from hexlet_django_blog.article.views import (IndexView, 
+                                              ArticleFormCreateView, 
+                                              ArticleCommentsView, 
+                                              ArticleView,
+                                              ArticleFormEditView,
+)
+
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(),
+         name='articles'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', ArticleView.as_view(),
          name='articles_index'),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
 from django.views import View
 from django.urls import reverse
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
+
 
 class IndexView(View):
 
@@ -30,12 +32,33 @@ def index(request):
     return HttpResponseRedirect(reverse('article', kwargs={'tags': 'python',
     'article_id': '42'}))
 
+class ArticleFormCreateView(View):
 
-#def get_article(request, tags, article_id):
-#    return render(
-#    request,
-#   'article/article.html', context={
-#    'tags': tags,
-#    'article_id': article_id
-#    }
-#)
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid(): # Если данные корректные, то сохраняем данные формы
+            form.save()
+            return redirect('articles') # Редирект на указанный маршрут
+        # Если данные некоректные, то возвращем человека обратно на страницу с заполенной формой
+        return render(request, 'articles/create.html', {'form': form})
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
